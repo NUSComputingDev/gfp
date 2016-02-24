f1e150b1146a52864038a9433ac543e64abff910
Syed Abdullah <syed@syedabdullah.net>
2016-02-24 17:35:43 +0800

    Added Models (Score and Game) for ScoreBoard app and removed some more Python bytecode from repo

--- scoreboard/models.py
+++ scoreboard/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# Model for Games that are played
+class Game(models.Model):
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField()
+
+# Score for the Game
+class Score(models.Model):
+    player = models.ForeignKey('players.Player', on_delete=models.CASCADE)
+    game = models.ForeignKey('Game', on_delete=models.CASCADE)
+    score = models.IntegerField(default=0)
