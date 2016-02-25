e233f258cd1539d8f47ef27247990116f40c8c7b
Syed Abdullah <syed@syedabdullah.net>
2016-02-25 11:23:50 +0800

    Added ModelAdmin for Game model, with a TabularInline for Score model

--- scoreboard/admin.py
+++ scoreboard/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Game, Score
 
-# Register your models here.
+class ScoreInline(admin.TabularInline):
+    model = Score
+
+class GameAdmin(admin.ModelAdmin):
+    inlines = [
+        ScoreInline,
+    ]
+
+admin.site.register(Game, GameAdmin)
