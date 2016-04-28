c68d74a50dfd30290d72be1ee93292a052962f5b
Syed Abdullah <syed@syedabdullah.net>
2016-04-29 00:24:17 +0800

    Added Session model for Games

--- scoreboard/admin.py
+++ scoreboard/admin.py
@@ -1,12 +1,33 @@
 from django.contrib import admin
-from .models import Game, Score
+from .models import Game, Score, GameSession
+from django.contrib.contenttypes.models import ContentType
+from django.core.urlresolvers import reverse
+from django.utils.safestring import mark_safe
 
 class ScoreInline(admin.TabularInline):
     model = Score
 
-class GameAdmin(admin.ModelAdmin):
+class GameSessionInline(admin.TabularInline):
+    model = GameSession
+    readonly_fields = ('total_participants', )
+
+    def total_participants(self, instance):
+        ct = ContentType.objects.get_for_model(instance)
+        url = reverse('admin:%s_%s_change' % (ct.app_label, ct.model), args=(instance.id,))
+        uri = '<a href="%s">%s</a>' % (url, instance.score_set.count())
+        return mark_safe(uri)
+
+    total_participants.allow_tags = True
+
+class GameSessionAdmin(admin.ModelAdmin):
     inlines = [
         ScoreInline,
     ]
 
+class GameAdmin(admin.ModelAdmin):
+    inlines = [
+        GameSessionInline,
+    ]
+
 admin.site.register(Game, GameAdmin)
+admin.site.register(GameSession, GameSessionAdmin)
--- scoreboard/models.py
+++ scoreboard/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 # Model for Games that are played
 class Game(models.Model):
@@ -7,10 +8,15 @@ class Game(models.Model):
     def __str__(self):
         return '%s' % (self.name)
 
-# Score for the Game
-class Score(models.Model):
-    player = models.ForeignKey('players.Player', on_delete=models.CASCADE)
+# Represents a Game Session for a Game!
+class GameSession(models.Model):
+    game_master = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     game = models.ForeignKey('Game', on_delete=models.CASCADE)
+
+# Score for a GameSession
+class Score(models.Model):
+    game_session = models.ForeignKey('GameSession', on_delete=models.CASCADE)
+    player = models.ForeignKey('players.Player', on_delete=models.CASCADE)
     score = models.IntegerField(default=0)
 
     class Meta:
