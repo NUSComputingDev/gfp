8dea8e106051bafc7eefbbcaf8b6cc09845f4fc4
Syed Abdullah <syed@syedabdullah.net>
2016-04-30 02:10:39 +0800

    Removed Points field from Player model and added human-readable representation of the model

--- players/models.py
+++ players/models.py
@@ -4,4 +4,6 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Player(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    points = models.PositiveIntegerField()
+
+    def __str__(self):
+        return '%s %s' % (self.user.first_name, self.user.last_name)
