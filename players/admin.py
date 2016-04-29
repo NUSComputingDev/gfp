94400db1c28d7c6400872d05950e10dae0442473
Syed Abdullah <syed@syedabdullah.net>
2016-04-30 02:11:05 +0800

    Added a dynamic total points calculator

--- players/admin.py
+++ players/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
+from django.db.models import Q
 from .models import Player
+from scoreboard.models import Score
 
 # Register your models here.
 @admin.register(Player)
 class PlayerAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('__str__', 'total_points', )
+    readonly_fields = ('total_points', )
+
+    def total_points(self, instance):
+        total = 0
+        games = Score.objects.filter(player=instance)
+        for game in games:
+            total += game.score
+        return total
