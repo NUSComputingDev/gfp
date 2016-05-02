ec1eac018be4c4f595797145951d2ff6ab846b43
Syed Abdullah <syed@syedabdullah.net>
2016-05-02 23:15:09 +0800

    Removed update view logic for Scoreboard app, as all updates will be done through the administrative panel. Furthermore, ensure that only logged in users can view the scoreboard.

--- scoreboard/views.py
+++ scoreboard/views.py
@@ -1,20 +1,9 @@
 from django.shortcuts import render
-from .models import Game
+from .models import Game, Score
 from django.contrib.auth.decorators import login_required, permission_required
 
 # Create your views here.
+@login_required
 def scoreboard_view(request):
 	games = Game.objects.all()
 	return render(request, 'scoreboard/scoreboard.html', {'games': games})
-
-@permission_required('scoreboard.can_update')
-@login_required
-def scoreboard_update(request):
-	if request.method =='POST':
-		# received an update, let's check
-		pass
-	else:
-		# show form to facilitator
-		pass
-
-	return render(request, 'scoreboard/update.html')
