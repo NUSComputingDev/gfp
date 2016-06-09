a0ba1e3972ab9788e39c1e7b07b01191d24fdc34
Syed Abdullah <syed@syedabdullah.net>
2016-06-10 01:08:25 +0800

    Removed reliance on Python calculated fields, in favour of SQL, so that the calculated fields can be sorted

--- players/admin.py
+++ players/admin.py
@@ -1,17 +1,43 @@
 from django.contrib import admin
-from django.db.models import Q
+from django.db.models import Q, F, Sum
+from django.db.models.expressions import RawSQL
 from .models import Player
-from scoreboard.models import Score
+from scoreboard.models import AggregatedScore, Score
 
 # Register your models here.
 @admin.register(Player)
 class PlayerAdmin(admin.ModelAdmin):
-    list_display = ('__str__', 'total_points', )
-    readonly_fields = ('total_points', )
+    list_display = ('__str__', 'normal_points', 'aggregated_points', 'total_points', )
+    readonly_fields = ('normal_points', 'aggregated_points', 'total_points', )
+
+    def get_queryset(self, request):
+        qs = super(PlayerAdmin, self).get_queryset(request)
+        qs = annotate_sum(qs, AggregatedScore, 'score', 'aggregated_points')
+        qs = annotate_sum(qs, Score, 'score', 'normal_points')
+        qs = qs.annotate(total_points=F('aggregated_points') + F('normal_points'))
+        return qs
+
+    def normal_points(self, instance):
+        return instance.normal_points
+
+    def aggregated_points(self, instance):
+        return instance.aggregated_points
 
     def total_points(self, instance):
-        total = 0
-        games = instance.score_set.all()
-        for game in games:
-            total += game.score
-        return total
+        return instance.total_points
+
+    total_points.admin_order_field = 'total_points'
+    aggregated_points.admin_order_field  = 'aggregated_points'
+    normal_points.admin_order_field = 'normal_points'
+
+def annotate_sum(qs, related_modelclass, field_name, annotation_name):
+        raw_query = """
+          SELECT SUM({field}) FROM {model} AS model
+            WHERE model.player_id = players_player.id
+        """.format(
+            field = field_name,
+            model = related_modelclass._meta.db_table,
+        )
+
+        annotation = {annotation_name: RawSQL(raw_query, [])}
+        return qs.annotate(**annotation)
