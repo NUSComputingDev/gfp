d52f181b65938c4a18449038fb9cb25be3965257
Syed Abdullah <syed@syedabdullah.net>
2016-06-10 01:09:56 +0800

    Added full support for users to input their Guesses for guess-based games

--- scoreboard/urls.py
+++ scoreboard/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.scoreboard_view),
-    url(r'^guessing$', views.guess_view),
+    url(r'^guessing-(?P<id>[0-9]+)$', views.guess_view, name='guessing-form'),
 ]
--- scoreboard/views.py
+++ scoreboard/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
-from .models import Game, Score
+from .models import Game, Score, Guess, GameSession
+from players.models import Player
 from .forms import GuessingForm
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required, permission_required
+from django.http import HttpResponseForbidden
+from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
 @login_required
@@ -10,6 +13,27 @@ def scoreboard_view(request):
 	return render(request, 'scoreboard/scoreboard.html', {'games': games})
 
 @login_required
-def guess_view(request):
+def guess_view(request, id):
+	associated_game_session = get_object_or_404(GameSession, pk=id)
 	form = GuessingForm()
+	if associated_game_session.game.game_type != Game.GUESSING:
+		return HttpResponseForbidden()
+	if request.method == 'POST':
+		try:
+			current_user_player = request.user.player
+		except Player.DoesNotExist:
+			current_user_player = None
+		if current_user_player:
+			guess_object, created = Guess.objects.get_or_create(player=current_user_player,
+														        game_session=associated_game_session)
+			form = GuessingForm(request.POST, instance=guess_object)
+			if form.is_valid():
+				new_guess = form.save()
+				messages.info(request, 'Your guess has been successfully recorded!')
+				return redirect('guessing-form', id=id)
+			else:
+				messages.error(request, 'Something went wrong!')
+				return redirect('guessing-form')
+		else:
+			messages.warning(request, 'You are most likely a cheeky admin!')
 	return render(request, 'scoreboard/guesser.html', {'form': form})
