8fd0c67f757aaa5e4ca63657ac3be9ec27382c78
Syed Abdullah <syed@syedabdullah.net>
2016-06-22 20:07:35 +0800

    Added Logout functionality and namespaced URL routes

--- players/urls.py
+++ players/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
-from django.contrib import admin
+from django.contrib.auth import views as auth_views
+
 
 from . import views
 
 urlpatterns = [
     url(r'^login$', views.login_view, name="players-login"),
+    url(r'^logout$', auth_views.logout, name="players-logout")
 ]
